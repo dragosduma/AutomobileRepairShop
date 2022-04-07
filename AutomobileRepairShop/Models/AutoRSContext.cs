e45cfad6ea22fe39d179f0200f10fa013d9228af
Ignat Eduard Gabriel <ignat.eduard1337@gmail.com>
2022-04-07 18:53:50 +0300

    models updated from database

--- AutomobileRepairShop/Models/AutoRSContext.cs
+++ AutomobileRepairShop/Models/AutoRSContext.cs
@@ -29,7 +29,7 @@ namespace AutomobileRepairShop.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-CL48MG8;Database=AutoRS;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=LAPTOP-8DFLJG73;Database=AutoRS;Trusted_Connection=True;");
             }
         }
 
@@ -42,6 +42,14 @@ namespace AutomobileRepairShop.Models
                 entity.Property(e => e.Id).HasColumnName("ID");
 
                 entity.Property(e => e.Date).HasColumnType("datetime");
+
+                entity.Property(e => e.IdUser).HasColumnName("ID_User");
+
+                entity.HasOne(d => d.IdUserNavigation)
+                    .WithMany(p => p.Appointments)
+                    .HasForeignKey(d => d.IdUser)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Appointment_User");
             });
 
             modelBuilder.Entity<Bill>(entity =>
