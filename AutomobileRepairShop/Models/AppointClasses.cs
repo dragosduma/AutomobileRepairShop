840491d8c69505610084609a9b3d431e86622113
Ignat Eduard Gabriel <ignat.eduard1337@gmail.com>
2022-04-12 19:16:09 +0300

    AppointClasses updated

--- AutomobileRepairShop/Models/AppointClasses.cs
+++ AutomobileRepairShop/Models/AppointClasses.cs
@@ -6,6 +6,7 @@
         public Car Car { get; set; }
         public User User { get; set; }
 
+        public AppointClasses() { }
         public AppointClasses(Appointment appointment, Car car, User user)
         {
             Appointment = appointment;
